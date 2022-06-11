d10b828c411071d6a711cd60f6fccc85d6ed3612
姚文强 <yaowenqiang111@163.com>
2022-06-11 14:57:51 +0800

    add poison pill tech demo

--- queue_demo.py
+++ queue_demo.py
@@ -6,9 +6,15 @@ def producer(queue):
         item = make_an_item_available()
         queue.put(item)
 
+    queue.put(0)
+
 def consumer(queue):
     while True:
         item = queue.get() # if queue is empty ,the get call will block
+        if item == 0:
+            # poison pill
+            queue.task_done()
+            break
         item = queue.get(block=False) # with the block=False flag, get call won't be blocked
         # do something with the item
         print(item)
@@ -24,6 +30,6 @@ t2 = Thread(target=consumer, args=(queue,))
 t1.start()
 t2.start()
 
-# t1.join()
-# t2.join()
+t1.join()
+t2.join()
 
